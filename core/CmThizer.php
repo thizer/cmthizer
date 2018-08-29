81fa9c3c24c00cabfcc4c20c1e9c1987afffd0be
Pernalonga <marco.a.braghim@gmail.com>
2018-08-28 22:48:07 -0300

    Ja conseguimos organizar os parametros necessarios para renderizar as paginas

--- core/CmThizer.php
+++ core/CmThizer.php
@@ -11,6 +11,8 @@ class CmThizer {
   
   private $post = array();
   
+  private $routes = array();
+  
   public function __construct() {
     try {
       
@@ -59,10 +61,55 @@ class CmThizer {
     return $this;
   }
   
-  private function resolveRoutes() {
+  private function resolveRoutes(): CmThizer {
     
-    dump(scandir('./site'));
+    $site = './site';
     
+    /**
+     * Nos da a lista de conteudo da pasta site
+     * 
+     * ## RECURSIVA ##
+     */
+    function scandirRecursive($dirname): array {
+      $items = array();
+      if (is_dir($dirname)) {
+        foreach (scandir($dirname) as $item) {
+          if (!in_array($item, array('.', '..'))) {
+            if (is_dir($dirname.DIRECTORY_SEPARATOR.$item)) {
+              $items[$dirname.DIRECTORY_SEPARATOR.$item] = scandirRecursive($dirname.DIRECTORY_SEPARATOR.$item);
+            } else {
+              $items[] = $item;
+            }
+          }
+        }
+      }
+      return $items;
+    }
+    $dirItems = scandirRecursive($site);
+    
+    /**
+     * Outra recursiva, agora para organizar os dados da pagina
+     * 
+     * ## RECURSIVA ##
+     */
+    function resolve(array $items) {
+      $routes = array();
+      foreach ($items as $folder => $content) {
+        if (is_dir($folder) && in_array('config.json', $content) && in_array('content.md', $content)) {
+
+          $config = json_decode(file_get_contents($folder.'/config.json'), true);
+          $config['content'] = $folder.'/content.md';
+
+          $routes[$config['uri']] = $config;
+        } else if(is_array($content)) {
+          $routes += resolve($content);
+        }
+      }
+      return $routes;
+    }
+    $this->routes = resolve($dirItems);
+    
+    return $this;
   }
 }
 
---
+++ site/blog/Dia de jogo no Thizer/content.md
@@ -0,0 +1,8 @@
+# Dia de jogo no Thizer
+Aqui temos um bom exemplo de como as coisas devem funcionar
+
+ 1. Usar um arquivo para landing pages
+ 1. Usar um arquivo para layout
+ 1. Uma pasta para paginas quaisquer
+ 1. Um arquivo config.yml vai determinar titulo, url e esses detalhes
+ 1. Nao vamos usar banco de dados
