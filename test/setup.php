687b014f9df4c78cb2efc028d61342b8d609fd2a
Marco A. Braghim <marco.a.braghim@gmail.com>
2018-09-10 16:47:15 -0300

    Setup travis

---
+++ test/setup.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Thizer Apps
+ */
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
+
+include_once __DIR__ . "/../core/CmThizer.php";
+
+// Para quando estamos mexendo diretamente no código
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+  $autoload = include __DIR__.'/../vendor/autoload.php';
+  $autoload->addPsr4('SuitUpTest\\', __DIR__.DIRECTORY_SEPARATOR.'.');
+  
+  // Para quando estamos alterando dentro do projeto que usa o SuitUp
+} else if (file_exists(__DIR__.'/../../../autoload.php')) {
+  $autoload = include __DIR__.'/../../../autoload.php';
+  $autoload->addPsr4('SuitUpTest\\', __DIR__.DIRECTORY_SEPARATOR.'.');
+  
+  // Para os testes dentro do Travis.ci
+} else {
+  echo "\n\nNão encontramos a pasta vendor, o sistema não presseguirá com os testes.\n\n";
+}
