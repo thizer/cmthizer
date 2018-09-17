2f633661116f606f74ec83b495dfbaacfd63e484
Marco A. Braghim <marco.a.braghim@gmail.com>
2018-09-17 11:44:26 -0300

    Some fixes on bugs and features. fixes #1

--- plugins/MenusPages.php
+++ plugins/MenusPages.php
@@ -2,18 +2,18 @@
 use CmThizer\Plugins\AbstractPlugin;
 
 class MenusPages extends AbstractPlugin {
-  
+
   /**
    * That is exactaly the right order which
    * this methods will be executed.
    */
-  
+
   public function preUri() {} // 1
   public function posUri() {} // 2
-  
+
   public function preParams() {} // 3
   public function posParams() {} // 4
-  
+
   public function prePost() {} // 5
   public function posPost() {} // 6
 
@@ -21,40 +21,40 @@ class MenusPages extends AbstractPlugin {
   public function posRoutes() {} // 8
 
   public function preRun() {
-    
+
     $sitePath = $this->getSitePath();
     $siteItems = scandir_recursive($sitePath);
-    
+
     // Organize site pages (and menus)
     $pages = array();
     $menus = array();
     foreach ($siteItems as $path => $content) {
-      
+
       // Ignora arquivos soltos na pasta
       if (is_array($content)) {
         foreach (array_keys($content) as $menuPath) {
           if (is_dir($menuPath)) {
-            
+
             // The menu name is the name of the parent folder
             $menuName = preg_replace("/^.+\//", '', $path);
-            
+
             // Load page configs
             $config = $this->getConfig($menuPath.'/config.json');
-            
+
             // Se eh uma pasta significa que temos uma subpagina
             if ($config->visible) {
               $url = ltrim(preg_replace("/\/{2,}/", '/', $config->uri), "/");
               $menus[$menuName][$url] = $config->title;
             }
-            
+
           } else if (file_exists($path.'/config.json') && is_readable($path.'/config.json')) {
-            
+
             // Get page configs
             $params = $this->getConfig($path.'/config.json');
-            
+
             // Check if page is defined as visible
             if ($params->visible) {
-              
+
               // Assoc file to the pages list
               $pages[$params->uri] = $params->title;
             }
@@ -62,10 +62,10 @@ class MenusPages extends AbstractPlugin {
         }
       }
     }
-    
+
     $this->addViewVar('pages', $pages);
     $this->addViewVar('menus', $menus);
-    
+
     // If theres a sub layout we will render it before
     // the main layout
     $route = $this->getCurrentRoute();
@@ -78,9 +78,9 @@ class MenusPages extends AbstractPlugin {
       }
     }
   }
-  
+
   public function posRun() {} // 10
-  
+
   private function getConfig(string $file): stdClass {
     $result = new stdClass();
     if (file_exists($file) && is_readable($file)) {
@@ -88,18 +88,18 @@ class MenusPages extends AbstractPlugin {
     } else {
       throw new Exception("The file '$file' was not found or is not readable");
     }
-    
+
     $result->uri = $result->uri ?? '/not-found';
     $result->title = $result->title ?? 'My Website';
     $result->visible = $result->visible ?? true;
-    
+
     return $result;
   }
-  
+
   private function renderSubLayout(array $route) {
-    
+
     $template = $this->getTemplate();
-      
+
     // Variables to be appended to the view
     foreach ($route as $varName => $varValue) {
       $$varName = $varValue;
@@ -108,9 +108,9 @@ class MenusPages extends AbstractPlugin {
     // Caminho base
     $basePath = $this->getBasePath();
     $baseUrl = $this->baseUrl();
-    
+
     $fileTypes = array('content.php', 'content.phtml', 'content.html');
-    
+
     $content = '';
     if (file_exists($route['dirname'].'/content.md')) {
       $parseDown = new ParsedownExtra();
@@ -125,20 +125,20 @@ class MenusPages extends AbstractPlugin {
         }
       }
     }
-    
+
     ob_start();
     if (file_exists($route['dirname'].'/sub-layout.phtml')) {
-      
+
       /** Render sub-layout from the root path **/
       include $route['dirname'].'/sub-layout.phtml';
-      
+
     } else if (file_exists(dirname($route['dirname']).'/sub-layout.phtml')) {
-      
+
       /** Render sub-layout from the parent path **/
       include dirname($route['dirname']).'/sub-layout.phtml';
     }
     $content = ob_get_clean();
-    
+
     $this->addViewVar('content', $content);
   }
 }
--- plugins/Sitemap.php
+++ plugins/Sitemap.php
@@ -2,42 +2,44 @@
 use CmThizer\Plugins\AbstractPlugin;
 
 class Sitemap extends AbstractPlugin {
-  
+
   public function preUri() {}
   public function posUri() {}
-  
+
   public function preParams() {}
   public function posParams() {}
-  
+
   public function prePost() {}
   public function posPost() {}
-  
+
   public function preRoutes() {}
   public function posRoutes() {
-    
+
     /**
      * Access to the URI /sitemap.xml
      */
-    
+
     if ($this->getUri()->getRouteName() == '/sitemap.xml') {
-      
+
       $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
       $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-      
-      foreach (array_keys($this->getRoutes()) as $uri) {
-        $sitemap .= '<url>';
-        $sitemap .= '<loc>'.$this->url($uri).'</loc>';
-        $sitemap .= '<changefreq>monthly</changefreq>';
-        $sitemap .= '<priority>'.(($uri == '/') ? '1.00' : '0.80').'</priority>';
-        $sitemap .= '</url>';
+
+      foreach ($this->getRoutes() as $uri => $route) {
+        if (!isset($route['visible']) || $route['visible']) {
+          $sitemap .= '<url>';
+          $sitemap .= '<loc>'.$this->url($uri).'</loc>';
+          $sitemap .= '<changefreq>monthly</changefreq>';
+          $sitemap .= '<priority>'.(($uri == '/') ? '1.00' : '0.80').'</priority>';
+          $sitemap .= '</url>';
+        }
       }
       $sitemap .= '</urlset>';
-      
+
       header("Content-type: application/xml");
       exit($sitemap);
     }
   }
-  
+
   public function preRun() {}
   public function posRun() {}
 }
