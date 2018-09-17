85ff1b56aee61307264d8d8fce5d1a113f2387d7
Marco A. Braghim <marco.a.braghim@gmail.com>
2018-09-17 15:33:41 -0300

    Robots txt

--- plugins/RobotsTxt.php
+++ plugins/RobotsTxt.php
@@ -2,43 +2,45 @@
 use CmThizer\Plugins\AbstractPlugin;
 
 class RobotsTxt extends AbstractPlugin {
-  
+
   public function preUri() {}
   public function posUri() {}
-  
+
   public function preParams() {}
   public function posParams() {}
-  
+
   public function prePost() {}
   public function posPost() {}
-  
+
   public function preRoutes() {}
   public function posRoutes() {
-    
+
     /**
      * Access to the URI /robots.txt
      */
-    
+
     if ($this->getUri()->getRouteName() == '/robots.txt') {
       $txt = "User-Agent: *\n\n";
+
+      // @todo: This verification is not working here
+      $sitemapPlugin = $this->getPlugin('Sitemap');
+      if ($sitemapPlugin && $sitemapPlugin->isActive()) {
+        $txt .= "sitemap: ".$this->url('/sitemap.xml')."\n\n";
+      }
+
+      $txt .= "Allow: *\n";
       $txt .= "Disallow: /core\n";
       $txt .= "Disallow: /docs\n";
       $txt .= "Disallow: /plugins\n";
       $txt .= "Disallow: /site\n";
       $txt .= "Disallow: /var\n";
       $txt .= "Disallow: /vendor\n\n";
-      
-      // @todo: This verification is not working here
-      $sitemapPlugin = $this->getPlugin('Sitemap');
-      if ($sitemapPlugin && $sitemapPlugin->isActive()) {
-        $txt .= "sitemap: ".$this->url('/sitemap.xml')."\n";
-      }
 
       header("Content-type: text/plain");
       exit($txt);
     }
   }
-  
+
   public function preRun() {}
   public function posRun() {}
 }
